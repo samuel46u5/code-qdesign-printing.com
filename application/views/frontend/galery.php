f3cc107d43dc386d17f9e20f0ee5ebccdefd8acd
samuel46u5 <49788892+samuel46u5@users.noreply.github.com>
2019-07-15 07:53:45 +0700

    Update galery.php

--- application/views/frontend/galery.php
+++ application/views/frontend/galery.php
@@ -10,41 +10,11 @@
     </div>
     <section class="bgwhite p-t-20 p-b-100">
         <div class="container">
-            <?php if ($this->session->flashdata('MSG')) { ?>
-                <?= $this->session->flashdata('MSG') ?>
-            <?php } ?>
-            <div class="bo17 w-size18 p-l-20 p-r-20 p-t-20 p-b-38 m-t-30 m-r-0 m-l-r-auto p-lr-15-sm">
-                <form class="login_form" action="<?php echo base_url('pages/order-search'); ?>" method="get">
-                    <h5 class="bo18 m-b-20 p-b-20 text-center">
-                        Konfirmasi Order
-                    </h5>
-
-                    <div class="flex-w flex-sb-m p-b-12 bo20">
-                        <div class="col-lg-12 form-group">
-                            <label>ID Order</label>
-                            <input class="form-control" type="text" name="idorder" required="">
-                        </div>
-                    </div>
-
-                    <div class="size15 trans-0-4 p-t-30 m-b-20">
-                        <button class="btn btn-block subs_btn form-control" type="submit">
-                            Cari
-                        </button>
-                    </div>
-                </form>
-            </div>
+            <h1> galery</h1>
         </div>
     </section>
     <?php echo $footer; ?>
-    <script>
-        function preview() {
-            var oFReader = new FileReader();
-            oFReader.readAsDataURL(document.getElementById("slip").files[0]);
-            oFReader.onload = function(oFREvent) {
-                document.getElementById("slippreview").src = oFREvent.target.result;
-            };
-        }
-    </script>
+
     </body>
 
     </html>
