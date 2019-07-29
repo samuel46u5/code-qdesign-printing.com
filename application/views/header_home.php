0f061786b83ec21e9a3ad4c3cc7c70486fe85fc6
samuel46u5 <49788892+samuel46u5@users.noreply.github.com>
2019-07-29 07:46:52 +0700

    ok

--- application/views/header_home.php
+++ application/views/header_home.php
@@ -87,26 +87,6 @@
 ?>
 
     <!--Start of Tawk.to Script-->
-
-    <!-- <?php if (!empty($tawkto)) { ?>
-                                                                        <script type="text/javascript">
-                                                                            alert('masuk');
-                                                                            var Tawk_API = Tawk_API || {},
-                                                                                Tawk_LoadStart = new Date();
-                                                                            (function() {
-                                                                                var s1 = document.createElement("script"),
-                                                                                    s0 = document.getElementsByTagName("script")[0];
-                                                                                s1.async = true;
-                                                                                s1.src = 'https://embed.tawk.to/' + <?= $tawkto->widgetScriptId; ?> + '/default';
-                                                                                // 5d159e9836eab9721119a32d
-                                                                                s1.charset = 'UTF-8';
-                                                                                s1.setAttribute('crossorigin', '*');
-                                                                                s0.parentNode.insertBefore(s1, s0);
-                                                                            })();
-                                                                        </script>
-    <?php } ?> -->
-    <!--End of Tawk.to Script-->
-
     <?php if (!empty($tawkto)) { ?>
         <script type="text/javascript">
             var Tawk_API = Tawk_API || {},
